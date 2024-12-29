0b7054a636d5079a348c6fdfe5e5ee1c0c3e19fb
Peter_V <root@Peters-Air.lan>
2024-12-28 21:14:50 -0800

    api call from component

--- vlamp-shop/src/App.jsx
+++ vlamp-shop/src/App.jsx
@@ -6,34 +6,15 @@ import GreetUser from "./components/greet-user/GreetUser.component";
 const App = () => {
   const [greeting, setGreeting] = useState("");
 
-  // name is the user input received from FormInput
+  // name is the user input received from GreetUser component
   const handleSayHello = (name) => {
-    // Data to be sent to the Backend
-    const data = {
-      user: name,
-    };
-
-    // Send data to the server
-    axios
-      .post("http://shop.localhost:80/server.php", data, {
-        headers: {
-          "Content-Type": "application/json", // Specify that we're sending JSON data
-        },
-      })
-      .then((response) => {
-        setGreeting(response.data); // Set the greeting text from the server
-      })
-      .catch((error) => {
-        console.error("Error during submit ...", error);
-        setGreeting("An error occurred, try again later...");
-      });
+    setGreeting(name);
   };
 
   return (
     <div className="App">
       <div className="main-container">
         <GreetUser sayHello={handleSayHello} />
-
         <div className="result-container">
           {/* Display the result from the server */}
           <h1>{greeting}</h1>
--- vlamp-shop/src/components/greet-user/GreetUser.component.jsx
+++ vlamp-shop/src/components/greet-user/GreetUser.component.jsx
@@ -1,17 +1,41 @@
 import { useState } from "react";
+import axios from "axios";
 
 const GreetUser = ({ sayHello }) => {
   const [name, setName] = useState("");
 
   // Handle input change
   const handleOnChange = (event) => {
+    // Update the 'name' state with the input field's value as user types
     setName(event.target.value);
   };
 
+  // Handle form submission
   const handleOnSubmit = (event) => {
+    // Prevents the default form submission behavior (page reload)
     event.preventDefault();
 
-    sayHello(name);
+    // Call the 'sayHello' function passed as a prop with the 'name' value
+    // sayHello(name);
+    // Data to be sent to the Backend
+    const data = {
+      user: name,
+    };
+
+    // Send data to the server
+    axios
+      .post("http://shop.localhost:80/server.php", data, {
+        headers: {
+          "Content-Type": "application/json", // Specify that we're sending JSON data
+        },
+      })
+      .then((response) => {
+        sayHello(response.data); // Set the greeting text from the server
+      })
+      .catch((error) => {
+        console.error("Error during submit ...", error);
+        sayHello("An error occurred, try again later...");
+      });
 
     setName("");
   };
